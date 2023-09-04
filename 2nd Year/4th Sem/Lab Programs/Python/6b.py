51255f0ac7f70b3423b93d54bcc7706f7ccbf34e
Vaishnav Sudheer <125580445+TheViper008@users.noreply.github.com>
2023-09-04 11:18:39 +0530

    Update 6b.py

--- 2nd Year/4th Sem/Lab Programs/Python/6b.py
+++ 2nd Year/4th Sem/Lab Programs/Python/6b.py
@@ -7,7 +7,7 @@ with ZipFile(myfile,"w") as Zip_object:
     for folder_name,sub_folders,file_names in os.walk(folder):
         for filename in file_names:
             filepath = os.path.join(folder_name,filename)
-        Zip_object.write(filepath,os.path.basename(filepath))
+            Zip_object.write(filepath,os.path.basename(filepath))
     if os.path.exists(myfile):
         print(f"{myfile} zipfile created")
     else:
