c7fef47fe3add4b48e25de373e2508509ecbf74a
Vaishnav Sudheer <125580445+TheViper008@users.noreply.github.com>
2023-09-21 23:26:47 +0530

    Update 6b.py

--- 2nd Year/4th Sem/Lab Programs/Python/6b.py
+++ 2nd Year/4th Sem/Lab Programs/Python/6b.py
@@ -1,14 +1,22 @@
 import os
-from zipfile import ZipFile 
+from zipfile import ZipFile
 
 folder = input("Enter folder name to be zipped:")
-myfile = folder+".zip"
-with ZipFile(myfile,"w") as Zip_object:
-    for folder_name,sub_folders,file_names in os.walk(folder):
-        for filename in file_names:
-            filepath = os.path.join(folder_name,filename)
-            Zip_object.write(filepath,os.path.basename(filepath))
+
+# Check if the specified folder exists
+if not os.path.exists(folder):
+    print(f"The folder '{folder}' does not exist.")
+else:
+    myfile = folder + ".zip"
+
+    # Check if the ZIP file already exists
     if os.path.exists(myfile):
-        print(f"{myfile} zipfile created")
+        print(f"The ZIP file '{myfile}' already exists.")
     else:
-        print("Not Found")
+        with ZipFile(myfile, "w") as Zip_object:
+            for folder_name, sub_folders, file_names in os.walk(folder):
+                for filename in file_names:
+                    filepath = os.path.join(folder_name, filename)
+                    Zip_object.write(filepath, os.path.basename(filepath))
+
+        print(f"{myfile} ZIP file created.")
