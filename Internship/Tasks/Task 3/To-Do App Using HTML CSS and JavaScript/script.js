f1a13bf3841a6b1fab892625b88eb5da54d2fc8a
Vaishnav Sudheer <125580445+TheViper008@users.noreply.github.com>
2023-10-31 21:21:20 +0530

    Add files via upload

---
+++ Internship/Tasks/Task 3/To-Do App Using HTML CSS and JavaScript/script.js
@@ -0,0 +1,48 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+  if (inputBox.value === "") {
+    alert("Please enter a task!!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    inputBox.value = "";
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  saveData();
+}
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+inputBox.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    addTask();
+    saveData();
+  }
+});
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTasks() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTasks();
