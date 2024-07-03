9b852c29a14cb233a69c86e04adcef0ca56be6d3
Vaishnav Sudheer <125580445+TheViper008@users.noreply.github.com>
2024-07-03 20:56:58 +0530

    Teapot

--- 3rd Year/6th Sem/CG Lab/PRG5/prg5.c
+++ 3rd Year/6th Sem/CG Lab/PRG5/prg5.c
@@ -37,7 +37,7 @@ glutSolidTeapot(0.09);
 glFlush();
 glLoadIdentity();
 }
-void main(int argc, char **argv)
+void main(int argc, char **argv) 
 {
 glutInit(&argc, argv);
 glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB|GLUT_DEPTH);
