b6b9a4a56393a05cbbb9af7122f4af5861d9a1d3
Vaishnav Sudheer <125580445+TheViper008@users.noreply.github.com>
2024-07-24 20:45:20 +0530

    Update views.py

--- 3rd Year/6th Sem/FSD Lab/PRG9/views.py
+++ 3rd Year/6th Sem/FSD Lab/PRG9/views.py
@@ -3,52 +3,44 @@ from django.http import HttpResponse
 from django.shortcuts import render
 import csv
 from csvapp.models import Student
-from reportlab.pdfgen import canvas
+from reportlab.platypus import SimpleDocTemplate, Table
 #Create your views here.
 
 
 def generate_csv_response(request):
-    queryset = Student.objects.all() # Get data from your models
+
+    fieldnames = ['student_name', 'student_usn']
+
+    queryset = Student.objects.all().values(*fieldnames)
+
     response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = f'attachment; filename="student_data.csv"'
-    writer = csv.writer(response)
-    # Write header row based on model fields using List comprehension
-    writer.writerow([field.name for field in queryset.model._meta.fields])
-    # Write data rows
-    for obj in queryset:
-        writer.writerow([getattr(obj, field.name) for field in queryset.model._meta.fields])
+    response['Content-Disposition'] = 'attachment; filename="student_data.csv"'
+
+    writer = csv.DictWriter(response, fieldnames=fieldnames)
+    writer.writeheader()
+
+    for row in queryset:
+        writer.writerow(row)
+
     return response
 
 def generate_pdf_response(request):
+    buffer = BytesIO()
+    pdf = SimpleDocTemplate(buffer)
+
     queryset = Student.objects.all()
-    response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = 'attachment; filename="student_pdfdata.pdf"'
-
-
-    # Create PDF document
-    pdf = canvas.Canvas(response)
-    y = 800 # Initial y position for writing text
-    pdf.setFont("Helvetica-Bold", 12)
-    pdf.drawString(100, y, "Student Data")
-
-
-    # Write data
-    pdf.setFont("Helvetica", 10)
-    y -= 30 # Move down for data rows
-
+    data = [['Name', 'USN']]  
 
     for obj in queryset:
-        data = f"Name: {obj.student_name}, USN: {obj.student_usn}" # Customize based on your model fields
-       
-        if y < 50:  # If we're near the bottom of the page, start a new page
-            pdf.showPage()
-            y = 800
-            pdf.setFont("Helvetica", 10)
-       
-        pdf.drawString(100, y, data)
-        y -= 15  # Move down for next row
+        data.append([obj.student_name, obj.student_usn])
 
+    table = Table(data)
 
-    pdf.showPage()
-    pdf.save()
+    pdf.build([table])
+
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="student_data.pdf"'
+    response.write(buffer.getvalue())
+    buffer.close()
+
     return response
